a00ee42d87d420d145dccab3f98a705efeab90e6
Colin Rundel <rundel@gmail.com>
2017-09-14 13:58:20 -0400

    Update

--- 2017-09-13-exercises.R
+++ 2017-09-13-exercises.R
@@ -3,7 +3,7 @@ library(nycflights13)
 
 flights = flights %>% tbl_df()
 
-# Demo 1 - 
+# Demo 1
 
 flights %>%
   filter(dest == "LAX") %>%                        # How many flights to Los Angeles (LAX) 
@@ -14,4 +14,34 @@ flights %>%
   filter(origin == "JFK") %>%                      # from JFK, and 
   filter(!is.na(air_time)) %>%
   summarize(n = n(), avg_dur = mean(air_time))              # what was their average duration?
+
+
+# Demo 2 - Skip
+
+
+# Exercise 1 - Which plane (check the tail number) flew out of each New York airport the most?
+
+flights %>%
+  filter(!is.na(tailnum)) %>%
+  group_by(tailnum, origin) %>% 
+  summarize(n = n()) %>%
+  group_by(origin) %>%
+  filter(n >= sort(n,decreasing = TRUE)[3]) %>%
+  ungroup() %>%
+  arrange(origin, desc(n))
+
+# Exercise 2 - What was the shortest flight out of each airport in terms of distance? In terms of duration?
+  
+flights %>%
+  filter(!is.na(distance)) %>%
+  group_by(origin) %>%
+  arrange(origin, distance) %>%
+  filter(distance == distance[1]) %>%
+  ungroup()
+
+flights %>%
+  filter(!is.na(air_time)) %>%
+  arrange(origin, air_time) %>%
+  filter(air_time == air_time[1]) %>%
+  ungroup()
 
