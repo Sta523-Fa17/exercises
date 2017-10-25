61372670e008580f009b02095c9f7b3dd93cea44
Colin Rundel <rundel@gmail.com>
2017-10-25 15:20:43 -0400

    Button

--- 2017-10-19-simple.R
+++ 2017-10-19-simple.R
@@ -2,18 +2,24 @@ library(shiny)
 
 options(shiny.reactlog=TRUE)
 
-colors = c("red","green","blue")
-
 shinyApp(
   ui =  fluidPage(
     titlePanel("Shiny Example - Reactivity"),
     mainPanel(
       sliderInput("number","Pick a number:", min = 0, max = 10, value = 5),
+      actionButton("button","Click me!"),
       textOutput("text")
     )
   ),
   server = function(input, output, session)
   {
+    observeEvent(
+      input$button,
+      {
+        updateSliderInput(session, "number", value = input$number+1)
+      }
+    )
+    
     output$text = renderText({
       input$number
     })
